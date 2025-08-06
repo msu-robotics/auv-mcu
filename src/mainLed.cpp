b728e44904c8168dd96607dc2ba7470d7f1358d6
Photon94 <299792458.photon.94@gmail.com>
2025-08-07 02:00:48 +0300

    Using FastLed lib

--- src/mainLed.cpp
+++ src/mainLed.cpp
@@ -1,14 +1,13 @@
 #include <Arduino.h>
-#include "led.h"
-
-LedIndicator led(100, 12);
-
+#include <FastLED.h>
+#define NUM_LEDS 10
+#define DATA_PIN 12
+CRGB leds[NUM_LEDS];
 void setup() {
-    led.setColor(0, 0, 255);
-    delay(1000);
-    led.fade(255, 0, 0, 2000);
+    Serial.begin(115200);
+    FastLED.addLeds<WS2812, DATA_PIN>(leds, NUM_LEDS);
 }
-
 void loop() {
-    led.update();
+	leds[0] = CRGB::White; FastLED.show(); delay(30);
+	leds[0] = CRGB::Black; FastLED.show(); delay(30);
 }
